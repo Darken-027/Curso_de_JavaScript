2f9915d0d19ad1abdb2ca937f04f20b23b17e786
Darken-027 <torut1638@gmail.com>
2025-10-30 17:31:34 -0500

    Se documentaron todos lo tipos de datos

---
+++ Basico/02-tiposDeDatos.js
@@ -0,0 +1,33 @@
+// Cadenas de texto (String)
+let nombre = "Bariszon";
+let alias = "Ahstell";
+let email = "torut1638@gmail.com";
+
+//Numeros (numbers)
+let edad = 23;
+let altura = 1.70;
+
+//Booleanos (Boolean)
+let esUnEstudiante = false;
+let esUnProfesor = true;
+
+//Dato no definido (undefined)
+let indefinido
+console.log(indefinido); // Muestra 'undefined' en la consola
+
+//valor nulo (null)
+let numeroNulo = null;
+
+//Symbol
+let MiSimbolo = Symbol("miSimboloUnico");
+
+// Gran Enero (BigInt)
+let myBigInt = 9007199254741991322354923856239562953692359265923569256;
+let myBigInt2 = 90071992547419999328386462321924672343342112837234724888823461n;
+
+//Mostramos lo tipos de datos en consola
+console.log(typeof nombre);
+console.log(typeof MiSimbolo);
+console.log(typeof numeroNulo);
+console.log(typeof esUnEstudiante);
+console.log(typeof null);
