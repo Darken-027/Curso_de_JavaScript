3f8987424b14c28161a1cdd6ccf0c1b758ae3924
Darken-027 <torut1638@gmail.com>
2025-10-30 16:18:15 -0500

    Creacion de los diferentes tipos de variables en JavaScript

---
+++ Basico/01-variables.js
@@ -0,0 +1,17 @@
+//VARIABLES
+
+//var
+var helloWorld = "Hello JavaScript";
+console.log(helloWorld);
+
+var helloWorld22 = "Hello new JavaScript";
+console.log(helloWorld22);
+
+//let
+let helloWorld1 = "Hello JavaScript";
+console.log(helloWorld1);
+
+//const
+
+const helloWorld3 = "Hello JavaScript4";
+console.log(helloWorld3);
