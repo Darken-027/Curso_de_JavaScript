54da92e24ae06db1a85c7d9e573d4e5350f7d29b
Darken-027 <torut1638@gmail.com>
2025-10-30 15:56:28 -0500

    Operadores aritmeticos

--- Basico/00-helloworld.js
+++ Basico/00-helloworld.js
@@ -12,3 +12,11 @@ console.log(`Hello World`);
 //comentario 2
 
 //tipos de datos
+cosole.log("5");
+console/log(5 + 2);
+console/log(5 - 2);
+console/log(5 * 2);
+console/log(5 / 2);
+console/log(5 % 2);
+console/log(5 ** 2);
+
