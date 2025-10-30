458b2e38db36df3e908fd47c27a9b329085992ef
Darken-027 <torut1638@gmail.com>
2025-10-30 15:53:43 -0500

    primer commit

---
+++ Basico/00-helloworld.js
@@ -0,0 +1,14 @@
+//comentario simple
+
+/*
+bloque de comentario
+*/
+
+console.log("Hello World");
+console.log('Hello World');
+console.log(`Hello World`);
+
+//coemtario 1
+//comentario 2
+
+//tipos de datos
