3e6b057f49522d1211c8beeade067bfb308912c1
Darken-027 <torut1638@gmail.com>
2025-12-03 15:06:00 -0500

    Se crearon he inicializaron diferentes tipos de variables

--- Basico/01-variables.js
+++ Basico/01-variables.js
@@ -31,3 +31,64 @@ let myDog = "Rover";
 console.log(myName);
 console.log(myAge);
 console.log(myDog);
+
+
+myName = "Ahstell"
+
+function logName(){
+    console.log(myName);
+}
+
+logName();
+
+//Error
+//var myName;
+
+
+//Se puede declarar la misma variables tantas veces se quiera usando var
+var myLastName = "Plaza";
+var myLastName = "Garcia";
+
+//Si se declaran varias variables con el mismo nombre usando let esto genera un error
+//let hola = "Chris";
+//let hola = "Bob";
+
+//ACTUALIZACION DE VARIABLES
+
+//Declaracion he inicializacion de una variable
+let namePerson = "Arelix";
+console.log(namePerson);
+
+//Actualizacion del valor de una variable
+namePerson = "Chris";
+//Mostrando el valor actualizado de una variable
+console.log(namePerson);
+
+
+//Creando variables
+let nacionalityPerson;
+let agePerson;
+let isMarriedPerson;
+let professionPerson;
+
+//TIPOS DE VARIABLES
+
+//Numericas
+let miEdad = 23;
+
+//Cadenas de caracteres (Strings)
+
+let dolphinGoodbye = "Hasta luego y gracias por todos los peces";
+
+//BOOLEANOS
+let iAmAlive = true;
+let iAmDead = false;
+console.log(iAmAlive);
+console.log(iAmDead);
+
+//Forma mas comun de usar booleanos
+
+let test = 6 < 3;
+console.log(test);
+
+
