75f2ff2c4fb781dc5795095b712becb8086ba6cd
Darken-027 <torut1638@gmail.com>
2025-12-03 14:19:45 -0500

    Trabajando con variables

--- Basico/00-HolaMundo.js
+++ Basico/00-HolaMundo.js
@@ -12,11 +12,12 @@ console.log(`Hello World`);
 //comentario 2
 
 //tipos de datos
-cosole.log("5");
-console/log(5 + 2);
-console/log(5 - 2);
-console/log(5 * 2);
-console/log(5 / 2);
-console/log(5 % 2);
-console/log(5 ** 2);
+console.log("5");
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(5 ** 2);
+console.log("Hola Ahstell");
 
--- Basico/01-variables.js
+++ Basico/01-variables.js
@@ -15,3 +15,19 @@ console.log(helloWorld1);
 
 const helloWorld3 = "Hello JavaScript4";
 console.log(helloWorld3);
+
+//Creacion de variables
+let myName;
+let myAge;
+
+//Inicializacion de variables
+myname = "Ahstell";
+myAge = 23;
+
+//Creacion he e inicializacion de una variable (Mas rapido)
+let myDog = "Rover";
+
+//Mostrar valores de variables
+console.log(myName);
+console.log(myAge);
+console.log(myDog);
