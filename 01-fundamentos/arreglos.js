3a221b342c69ce59c277e84ecaa078ee9666360c
Darken-027 <torut1638@gmail.com>
2026-04-06 22:45:57 -0500

    Cambios guardados

---
+++ 01-fundamentos/arreglos.js
@@ -0,0 +1,31 @@
+
+
+// const arr = new Array(10);
+// console.log(arr);
+// const arr = [];
+
+
+let videojuegos = ['Halo', 'War Thunder', 'Hell Let Loose'];
+console.log({videojuegos});
+
+console.log(videojuegos[0]);
+
+let arreglocosas = [
+    true,
+    123,
+    'fernando',
+    1 + 3,
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01-fundamentos/primitivos.js
@@ -0,0 +1,40 @@
+let nombre = 'Piter parker';
+console.log(nombre);
+
+nombre = 'Ben parken';
+
+console.log(nombre);
+
+nombre = "Tia May";
+nombre = `Tia May`;
+
+console.log( typeof nombre );
+console.log(typeof(nombre));
+
+let esMarvel = true;
+console.log(typeof esMarvel);
+
+let edad = 45;
+console.log(typeof(edad));
+
+edad = 3.33000;
+console.log(typeof(edad));
+console.log(edad);
+
+//CamelCase
+let superPoderDeSpiderMan;
+
+
+//SnakeCase
+let super_poder;
+console.log(typeof super_poder);
+
+let soyNull = null;
+console.log(typeof soyNull);
+
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+console.log(typeof symbol1);
+
+console.log( symbol1 === symbol2);
