6fc85c1a61394e7dc146b1d6be11037b17babe7c
Darken-027 <torut1638@gmail.com>
2026-04-06 22:50:36 -0500

    Se reaizaron cambios

--- 01-fundamentos/arreglos.js
+++ 01-fundamentos/arreglos.js
@@ -5,17 +5,39 @@
 // const arr = [];
 
 
-let videojuegos = ['Halo', 'War Thunder', 'Hell Let Loose'];
-console.log({videojuegos});
+let juegos = ['Halo', 'War Thunder', 'the las of us', 'Gear of War'];
+console.log('Largo', juegos.length);
 
-console.log(videojuegos[0]);
+let primero = juegos[2-2];
+let ultimo = juegos[juegos.length - 1];
 
-let arreglocosas = [
-    true,
-    123,
-    'fernando',
-    1 + 3,
-];
+console.log({primero, ultimo});
+
+
+juegos.forEach((elemento, indice, arr) => {
+    console.log({elemento, indice, arr});
+});
+
+
+let nuevaLongitud = juegos.push('Zelda');
+console.log(nuevaLongitud, juegos);
+
+nuevaLongitud = juegos.unshift('Fire Emblem');
+console.log({nuevaLongitud, juegos});
+
+juegosBorrados = juegos.pop();
+console.log({juegosBorrados, juegos});
+
+console.log(juegos);
+let pos = 1;
+let juegoBorrado = juegos.splice(pos, 2);
+console.log({ juegoBorrado, juegos });
+
+let gearofwarindex = juegos.indexOf('the las of us');//CaSeSensItIvE => las mayuculas y minusculas hacen la diferencia
+console.log({gearofwarindex});
+
+
+//TODO: Referencia
 
 
 
